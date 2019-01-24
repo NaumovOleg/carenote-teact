a9fed9110e618366e88ce9bae69c9dd4b97da4d3
Oleh <keeperoleg26@gmail.com>
2019-01-24 17:58:20 +0200

    Added pages .

---
+++ src/components/root/main-components/Subscriptions.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+
+class Subscriptions extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="subscriptions-component">
+                Subscriptions
+            </div>
+        );
+    }
+}
+const mapStateToProps = state => {
+    return state;
+};
+
+const mapDispatchToProps = dispatch => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Subscriptions);
