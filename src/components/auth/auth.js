ad1701f492a8d191270dcf18d44c6265c8064978
Unknown <dara.klymenko@gmail.com>
2019-01-25 13:30:10 +0200

    сщььше

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,11 +1,13 @@
 
 import React, { Component } from 'react';
+import '../../styles/auth.css'
 class Auth extends Component {
 
     render () {
         return (
             <div className="auth-container">
                 auth page
+                
             </div>)
     }
 }
