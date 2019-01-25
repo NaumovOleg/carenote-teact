4dbe3309adef2301fd5a96ddbd584fe49d2d2fce
Oleh <keeperoleg26@gmail.com>
2019-01-25 13:09:29 +0200

    login page

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,12 +1,20 @@
-
 import React, { Component } from 'react';
+import Login from './login';
 class Auth extends Component {
 
+
     render () {
+        const loginData = {
+
+            header:    'Hi. Welcome back.',
+            subHeader: 'Enter your email and password to login to your account.',
+            registerHere:'',
+            login:''
+        };
         return (
-            <div className="auth-container">
-                auth page
-            </div>)
+            <div  className="auth-container">
+                <Login data={loginData}  />
+            </div>);
     }
 }
 
---
+++ src/components/auth/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Login extends Component {
+
+    render () {
+        return (
+            <div className="login-container">
+                {this.props.data.header}dddd
+                {this.props.data.subHeader}
+
+            </div>)
+    }
+}
+
+export default Login;
