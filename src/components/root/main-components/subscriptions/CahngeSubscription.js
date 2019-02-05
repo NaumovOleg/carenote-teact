dd129b3d0424973d7e5c45197724f84ae203a102
Dara <dara.klymenko@gmail.com>
2019-02-05 17:24:08 +0200

    change subscription page layout

--- src/components/root/main-components/subscriptions/CahngeSubscription.js
+++ src/components/root/main-components/subscriptions/CahngeSubscription.js
@@ -9,7 +9,28 @@ class ChangeSubscription extends Component {
 
         return (
             <div className="change-subscriptions-component">
-              change subscriptions
+              <div className="change-subscriptions-container">
+                 <div className="change-subscriptions-header">
+                    <p>Change Subscription</p>
+                    <p><a href="#">&#60; Back</a></p>
+                 </div>
+                 <div className="current-subscription">
+                    <p><strong>Current Subscription: Gold Plan</strong><br />
+                    2 Care calls per week, 2 outbound calls, unlimited text messaging</p>
+                    <p>$89/mo</p>
+                 </div>
+                 <div className="silver-plan-subscription">
+                    <p><strong>Silver Plan $49/mo</strong><br />
+                    1 Care call per week, unlimited text messaging</p>
+                    <button><a href="#">Change to Silver</a></button>
+                 </div>
+                 <div className="platinum-plan-subscription">
+                    <p><strong>Platinum Plan $299/mo</strong><br />
+                    7 Care calls per week, unlimited outbound calls,
+                    unlimited text messaging</p>
+                    <button><a href="#">Change to Platinum</a></button>
+                 </div>
+              </div>
             </div>
         );
     }
